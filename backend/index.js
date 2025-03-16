c9be187acafedcf61dc929d9bb59b82b462e1c43
BhawarSuthar7023 <bhawarsuthar7023@gmail.com>
2025-03-16 17:51:48 +0530

    add code latest 1

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const recipeLimiter = rateLimit({
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5174","https://webledger-eight.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173","https://webledger-eight.vercel.app"];
 app.use(
   cors({
     origin: allowedOrigins,
